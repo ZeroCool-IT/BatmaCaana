741e243e31cbb608c6f66a2a88f774499a98dbc6
Marco Battisti <marco.battisti83@gmail.com>
2015-05-07 15:30:50 +0200

    Changed link to match Locale

--- app/src/main/java/it/zerocool/batmacaana/GcmRegistrationAsyncTask.java
+++ app/src/main/java/it/zerocool/batmacaana/GcmRegistrationAsyncTask.java
@@ -35,7 +35,7 @@ class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
     @Nullable
     private static Registration regService = null;
     private final Context context;
-    private final boolean test = true;
+    private final boolean test = false;
     private GoogleCloudMessaging gcm;
 
     public GcmRegistrationAsyncTask(Context context) {
